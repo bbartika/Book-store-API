f4f28162941c6d8d5ddfee7bae2a4a8b5eca1877
bbartika <137605070+bbartika@users.noreply.github.com>
2024-09-20 10:36:17 +0530

    implement register and login functionalities on user

--- controller/user.js
+++ controller/user.js
@@ -1,9 +1,7 @@
 const { User } = require('../models')
 const bcrypt=require('bcrypt')
 
-
-
-
+const jwt=require('jsonwebtoken')
 
 
 function isStringInvalid(string){
@@ -43,6 +41,8 @@ exports.Register=async(req,res)=>{
    
 }
 
+
+
 exports.logIn = async (req, res) => {
     try {
         const{useremail,userpassword}  = req.body
@@ -57,7 +57,9 @@ exports.logIn = async (req, res) => {
         if (user) {
             const passwordMatch = await bcrypt.compare(userpassword, user.password);
             if (passwordMatch) {
-                res.status(200).json({ message: "User logged in successfully"});
+                res.status(200).json({ message: "User logged in successfully",
+                    token:generateAccessToken(user.id,user.name,user.email)
+                });
             } else {
                 res.status(400).json({ error: 'Invalid password' });
             }
@@ -67,4 +69,8 @@ exports.logIn = async (req, res) => {
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
-}
+};
+
+function generateAccessToken(id,username,email){
+    return jwt.sign({userid:id,username:username,useremail:email},'a1aa9904fd5e3367d2e6bb6e63d31416e2b214fcb0bbafadd3bcc9ed524bb22fbdf6948669b7a29b79b77ed24db4e3fc5e3ea39af09e021de696c6dfdeac0ac8', { expiresIn: '1h' });
+};
